134b2a820ab487dad02ad7877e4d081e38275b41
Yankeesti <timheinsberg@web.de>
2022-11-14 22:17:35 +0100

    Update LearningPhase

--- Code/src/defaultPackage/LearningPhase.java
+++ Code/src/defaultPackage/LearningPhase.java
@@ -1,5 +1,46 @@
 package defaultPackage;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class LearningPhase {
 
+	Date started;
+	Date ended;
+	long learned;// Time learned in Seconds
+	ArrayList<String> notes;
+	
+	public LearningPhase() {
+		started = getAktDate();
+		notes = new ArrayList<String>();
+	}
+	
+	public void finish() {
+		ended = getAktDate();
+		learned =  (ended.getTime()-started.getTime())/1000;
+		
+	}
+
+	public void addNote(String note) {
+		notes.add(note);
+	}
+	
+	public long getLearned() {
+		return learned;
+	}
+
+	private Date getAktDate() {
+		Date outPut = new Date();
+		LocalDateTime now = LocalDateTime.now();  
+		
+		outPut.setYear(now.getYear()-1900);
+		outPut.setMonth(now.getMonthValue());
+		outPut.setHours(now.getHour());
+		outPut.setMinutes(now.getMinute());
+		outPut.setSeconds(now.getSecond());
+		
+		return outPut;
+	}
+	
 }
--- Code/src/defaultPackage/Main.java
+++ Code/src/defaultPackage/Main.java
@@ -1,54 +1,25 @@
 package defaultPackage;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Date eins = new Date(122, 9, 2);
-		Date zwei = new Date(122, 10, 2);
-		
-		System.out.println(eins.toString());
-		System.out.println(zwei.toString());
-		
-		System.out.println(berechnungWochenAnzahl(eins,zwei));
-	}
-
-	/**
-	 * @param semesterStart
-	 * @param semesterEnd
-	 * @return anzahl ganzer Wochen im angegeben Zeitraum
-	 */
-	private static int berechnungWochenAnzahl(Date semesterStart, Date semesterEnd) {
-		Date startMonday = getMonday(semesterStart);
-		Date endMonday = getMonday(semesterEnd);
-		
-		
-		int anzahl = 0;
-		System.out.println(startMonday.compareTo(endMonday));
-		while(startMonday.compareTo(endMonday)<= 0) {
-			startMonday.setDate(startMonday.getDate()+7);
-			anzahl ++;
+		LearningPhase test = new LearningPhase();
+		try {
+			Thread.sleep(2300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return anzahl;
+		test.finish();
+		
+		System.out.println(test.getLearned());
+		
 	}
 	
-	/**
-	 * @param tag
-	 * @return denn Montag welcher in der gleichen woche liegt wie tag
-	 */
-	private static Date getMonday(Date tag) {
-		//Montag finden
-				Date Monday;
-				int day = tag.getDay();
-				
-				//Da in der Date Klasse Sonntag an stelle 0 ist rücken wir hiermit den Montag auf stelle null
-				if(day == 0)// wenn sonntag = 6
-					day = 6;
-				else //initial für tag um eins nach hinten schieben um montag an 0 zu haben
-					day -= 1;
-				
-				Monday = new Date(tag.getYear(),tag.getMonth(),tag.getDate()-day);
-				return Monday;
-	}
+	
+
+	
 }
