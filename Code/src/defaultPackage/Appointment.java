7ce9fa748c57d8474a97e4d414031cfaf08c2fe2
Yankeesti <timheinsberg@web.de>
2022-11-15 11:47:00 +0100

    finsihed Main SetUp
    Das Grundgerüst ist Fertig

--- Code/src/defaultPackage/Appointment.java
+++ Code/src/defaultPackage/Appointment.java
@@ -1,5 +1,16 @@
 package defaultPackage;
 
-public class Appointment {
+import java.util.Date;
 
+public class Appointment {
+	Date date;
+	String description;
+	
+	public Appointment(Date date, String description) {
+		this.date = date;
+		this.description = description;
+	}
+	
+	public Date getDate() {return date;}
+	public String getDescribtion() {return description;}
 }
